a5af61afc563ddeec7d7cd9567713fca015f14ae
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-03-26 12:59:31 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 28228907
-api_hash = os.environ.get("API_HASH", "3848da16c807890cabc8d1395248c9cf3")
+api_id = 24400704
+api_hash = os.environ.get("API_HASH", "985be086326f373e026ea06e5fdef026")
 bot_token = os.environ.get("BOT_TOKEN")
-auth_users = os.environ.get("AUTH_USERS", "6127558921")
+auth_users = os.environ.get("AUTH_USERS", "7150812859")
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "6127558921")
+osowner_users = os.environ.get("OWNER_USERS", "7150812859")
 owner_users = [int(num) for num in osowner_users.split(",")]
